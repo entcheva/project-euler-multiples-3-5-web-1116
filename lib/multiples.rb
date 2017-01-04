009f07c8e3b2a58a924085e8216a824731c8fd23
entcheva <christina.entcheva@gmail.com>
2017-01-04 09:44:40 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,29 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  multiples = []
+
+  i = 1
+  while i < limit
+    if (i % 3 == 0) || (i % 5 == 0)
+      multiples << i
+    end
+    i += 1
+  end
+  multiples
+
+end
+
+
+
+def sum_multiples(limit)
+  sum = 0
+
+  i = 0
+  while i < limit
+
+    i += 1
+  end
+
+
+
+
+end
